ac3278b9369fd123321112ac1960ead8a44bd160
shycity <nickalt@gmail.com>
2015-11-20 11:58:31 -0600

    Change column order, column names and other updates.

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
-#Human-Activity-Recognition-Using-Smartphones-Dataset-Summary
+#Human Activity Recognition Using Smartphones Dataset Summary
 
-This repository contains R code (run_analysis.R) that generates summary data (average_subject_activity.txt) from the [Human Activity Recognition Using Smartphones Data Set](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones).
+This repository contains R code (run_analysis.R) that generates summary data (activity_subject_averages.txt) from the [Human Activity Recognition Using Smartphones Data Set](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones).
 
-A description of the output file can be found in CodeBook.md
+##Contents
+* README.md - This file
+* run_analysis.R - Reads in source data and writes out activity_subject_averages.txt
+* activity_subject_averages.txt - Produced by running run_analysis.R. Contents of file are described in CodeBook.md
+* CodeBook.md - Describes the contents of activity_subject_averages.txt
 
-##Requirments
+##Requirements
 * R
 * Lots of different R libraries
 
 ##Usage
 Run run_analysis.R
+
+##What it does
+
+Running the run_analysis.R file performs a number of operations:
+
+Downloads and extracts data from http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+Merges the training and the test sets to create one data set.
+Extracts only the measurements on the mean and standard deviation for each measurement. 
+Uses descriptive activity names to name the activities in the data set
+Appropriately labels the data set with descriptive variable names. 
+Writes out a new data set containing the averages of each variable for each activity and each subject.
--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,17 @@
-#Merges the training and the test sets to create one data set.
-#Extracts only the measurements on the mean and standard deviation for each measurement. 
-#Uses descriptive activity names to name the activities in the data set
-#4. Appropriately labels the data set with descriptive variable names. 
-#From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# See README.md for a description of the code
+# See CodeBook.md for a description of the output file
 
-#data set description
-#http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
 
+# Set working directory
 setwd("C:/n/Google Drive/School/Coursera/Getting and Cleaning Data/Course Project/Human-Activity-Recognition-Using-Smartphones-Dataset-Summary")
-dir.create("data")
+
+# Create a data folder if one does not exist and change to that folder
+if(!file.exists("./data")) {
+  dir.create("./data")
+}
 setwd("data")
 
+# Download the source data file if it has not already been downloaded and decompress the data
 filename <- "getdata-projectfiles-UCI HAR Dataset.zip"
 
 if(!file.exists(filename)) {
@@ -20,24 +21,15 @@ if(!file.exists(filename)) {
 
 unzip(filename)
 
-#561 columns, each 16 characters wide
-col_widths <- rep(16,561)
 
-#library that's fast at reading fixed with columns
-library(readr)
+col_widths <- rep(16,561) # 561 columns, each 16 characters wide
 
-#reasonably fast method way to read fixed width files
+# Read data file using read_fwf. read_fwf seemed to be much faster than read.fwf
+library(readr) # Load library that's fast at reading fixed with columns
 X_test <- read_fwf(file="UCI HAR Dataset/test/X_test.txt", fwf_widths(col_widths))
 X_train <- read_fwf(file="UCI HAR Dataset/train/X_train.txt", fwf_widths(col_widths))
 
-#uses gigabyte of memory to read file
-#X_test <- read.fwf(file="UCI HAR Dataset/test/X_test.txt",widths=col_widths)
-#X_train <- read.fwf(file="UCI HAR Dataset/train/X_train.txt",widths=col_widths)
-
-#results in different numbers of columns because of varying amounts of spaces between columns
-#X_test <- read.csv("UCI HAR Dataset/test/X_test.txt", header = FALSE, sep = " ", colClasses = "character")
-#X_train <- read.csv("UCI HAR Dataset/train/X_train.txt", header = FALSE, sep = " ", colClasses = "character")
-
+# Read in other data files
 Y_test <- read.csv("UCI HAR Dataset/test/Y_test.txt", header = FALSE, sep = " ", colClasses = "character")
 Y_train <- read.csv("UCI HAR Dataset/train/Y_train.txt", header = FALSE, sep = " ", colClasses = "character")
 
@@ -46,37 +38,45 @@ subject_train <- read.csv("UCI HAR Dataset/train/subject_train.txt", header = FA
 
 activity_labels <- read.csv("UCI HAR Dataset/activity_labels.txt", header = FALSE, sep = " ", colClasses = "character")
 
-#join test and train data
+# Join the test and train data into a single data frame for each type
 X_full <- rbind(X_train, X_test)
 Y_full <- rbind(Y_train, Y_test)
 subject_full <- rbind(subject_train, subject_test)
 
-#delete variables that are no longer needed
+# Delete temporary variables that are no longer needed
 rm(X_test, Y_test, X_train, Y_train, subject_test, subject_train)
 
-#merge meaningful names into Y_full
+# Merge meaningful field names into Y_full from activity_labels
 activities <- merge(Y_full,activity_labels,by="V1")
 
-#get names of all fields from features.txt
+# Read in names of all fields from features.txt
 features <- read.csv("UCI HAR Dataset/features.txt", header = FALSE, sep = " ", colClasses = "character")
 
-#select only the columns that are mean ("-mean()") or standard deviation ("-std()")
+# Select only the columns that are mean ("-mean()") or standard deviation ("-std()")
 selected_features <- subset(features, grepl("-mean\\(\\)|-std\\(\\)", features[[2]]))
 mean_and_std_column_list <- as.numeric(selected_features[[1]])
 
-#make nicer names for column names
+# Make column names easier to read
 measurement_column_names <- make.names(selected_features[[2]], unique = TRUE)
 
-#prepend the subject and activity labels column ahead the meand and std columns we want from the X_full data
-full_mean_and_std <- cbind(subject_full, activities[2], X_full[mean_and_std_column_list])
+measurement_column_names <- gsub("\\.+", ".", measurement_column_names) # Remove consective periods
+measurement_column_names <- gsub("\\.$", "", measurement_column_names) # Remove trailing periods
+measurement_column_names <- gsub("BodyBody", "Body", measurement_column_names) # Remove doubled "BodyBody" text
 
-#delete variables that are no longer needed
-rm(X_full, Y_full, subject_full, X_full_mean_and_std)
+# Prepend the activity and subject labels columns ahead the mean and std columns we want from the X_full data
+full_mean_and_std <- cbind(activities[2], subject_full, X_full[mean_and_std_column_list])
 
-#add column names to data frame
-colnames(full_mean_and_std) <- c("subject", "activity", measurement_column_names)
+# Delete variables that are no longer needed
+rm(X_full, Y_full, subject_full)
 
-#From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-average_subject_activity <- aggregate(. ~ subject + activity, full_mean_and_std, mean)
+# Add column names to data frame
+colnames(full_mean_and_std) <- c("activity", "subject", measurement_column_names)
 
-write.table(average_subject_activity, file = "../average_subject_activity.txt", quote = FALSE, row.names = FALSE)
+# Create a new dataframe with the average of each variable for each activity and each subject.
+activity_subject_averages <- aggregate(. ~ activity + subject, full_mean_and_std, mean)
+
+# Sort the data frame
+activity_subject_averages <- activity_subject_averages[with(activity_subject_averages, order(activity, subject)), ]
+
+# Write out the data
+write.table(activity_subject_averages, file = "../activity_subject_averages.txt", quote = FALSE, row.names = FALSE)
